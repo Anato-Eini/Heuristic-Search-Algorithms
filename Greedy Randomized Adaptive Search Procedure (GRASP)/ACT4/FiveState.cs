d8af1f2ba63b280e92a128a2246ee9acc8312fbf
Fleurette <135421389+Anato-Eini@users.noreply.github.com>
2024-10-03 14:49:14 +0800

    To be implemented

---
+++ Greedy Randomized Adaptive Search Procedure (GRASP)/ACT4/FiveState.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ACT4
+{
+    class FiveState
+    {
+        public int[] Y = new int[5];
+
+        public FiveState() { }
+        public FiveState(int a)
+        {
+            Y[0] = Y[1] = Y[2] = Y[3] = Y[4] = a;
+        }
+        public FiveState(int a, int b, int c, int d, int e)
+        {
+            Y[0] = a;
+            Y[1] = b;
+            Y[2] = c;
+            Y[3] = d;
+            Y[4] = e;
+        }
+        public FiveState(FiveState f)
+        {
+            Y[0] = f.Y[0];
+            Y[1] = f.Y[1];
+            Y[2] = f.Y[2];
+            Y[3] = f.Y[3];
+            Y[4] = f.Y[4];
+        }
+    }
+}
---
+++ Greedy Randomized Adaptive Search Procedure (GRASP)/ACT4/SixState.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ACT4
+{
+    class SixState
+    {
+        public int[] Y = new int[6];
+
+        public SixState() { }
+        public SixState(int a)
+        {
+            Y[0] = Y[1] = Y[2] = Y[3] = Y[4] = Y[5] = a;
+        }
+        public SixState(int a, int b, int c, int d, int e, int f)
+        {
+            Y[0] = a;
+            Y[1] = b;
+            Y[2] = c;
+            Y[3] = d;
+            Y[4] = e;
+            Y[5] = f;
+        }
+        public SixState(SixState f)
+        {
+            Y[0] = f.Y[0];
+            Y[1] = f.Y[1];
+            Y[2] = f.Y[2];
+            Y[3] = f.Y[3];
+            Y[4] = f.Y[4];
+            Y[5] = f.Y[5];
+        }
+    }
+}
